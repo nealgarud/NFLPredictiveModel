dcd86a37963401a74ab3da8ae13d0f9eb47a8df6
nealgarud76 <neal.garud@ZDMA.onmicrosoft.com>
2026-01-09 12:13:51 -0500

    Create playerimpactdata module for game-level injury impact processing

---
+++ PredictiveDataModel/SupervisedLearningDataCollector/playerimpactdata/__init__.py
@@ -0,0 +1,14 @@
+"""
+PlayerImpactData Module
+
+Processes game-level player impact data by combining:
+- Position mapping (PositionMapper)
+- Player weights (PlayerWeightAssigner)
+- Injury impact calculation (InjuryImpactCalculator)
+- Sportradar API data (SportradarClient)
+
+Main entry point: GameProcessor
+"""
+
+__version__ = "0.1.0"
+
